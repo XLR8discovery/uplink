9ab46f71eab34d8c7d2078a54d48dcc3680a6938
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2025-08-01 08:45:22 -0400

    Add files via upload

---
+++ common.go
@@ -0,0 +1,11 @@
+// Copyright (C) 2025 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package ecclient
+
+import (
+	"github.com/zeebo/errs"
+)
+
+// Error is the errs class of standard Ranger errors.
+var Error = errs.Class("ecclient")
