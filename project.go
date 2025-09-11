66b59ba2b784075d167f9166c9e66cc4b921b021
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2025-09-11 06:57:08 -0400

    Add files via upload

---
+++ project.go
@@ -0,0 +1,117 @@
+// Copyright (C) 2025 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package project
+
+import (
+	"context"
+	"errors"
+	_ "unsafe" // for go:linkname
+
+	"github.com/spacemonkeygo/monkit/v3"
+	"github.com/zeebo/errs"
+
+	"xlr8d.io/common/pb"
+	"xlr8d.io/common/signing"
+	"xlr8d.io/common/uuid"
+	"xlr8d.io/uplink"
+	"xlr8d.io/uplink/internal/expose"
+	"xlr8d.io/uplink/private/metaclient"
+)
+
+var (
+	mon          = monkit.Package()
+	packageError = errs.Class("project")
+)
+
+var (
+	// ErrProjectNoLock is returned when a project has object lock disabled.
+	ErrProjectNoLock = errors.New("object lock is not enabled for this project")
+
+	// ErrLockNotEnabled is returned when object lock is disabled as a feature.
+	ErrLockNotEnabled = errors.New("object lock is not enabled")
+)
+
+// EdgeURLOverrides contains the URL overrides for the edge service.
+type EdgeURLOverrides struct {
+	AuthService         string
+	PublicLinksharing   string
+	InternalLinksharing string
+}
+
+// Info contains the public ID, salt and edge url overrides of a project.
+type Info struct {
+	PublicId         uuid.UUID
+	Salt             []byte
+	EdgeUrlOverrides *EdgeURLOverrides
+}
+
+func getProjectInfo(ctx context.Context, config uplink.Config, access *uplink.Access) (_ *pb.ProjectInfoResponse, err error) {
+	dialer, err := expose.ConfigGetDialer(config, ctx)
+	if err != nil {
+		return nil, packageError.Wrap(err)
+	}
+	defer func() { err = errs.Combine(err, dialer.Pool.Close()) }()
+
+	client, err := metaclient.DialNodeURL(ctx, dialer, access.SatelliteAddress(), expose.AccessGetAPIKey(access), config.UserAgent)
+	if err != nil {
+		return nil, packageError.Wrap(err)
+	}
+	defer func() { err = errs.Combine(err, client.Close()) }()
+
+	info, err := client.GetProjectInfo(ctx)
+	return info, err
+}
+
+// GetPublicID gets the public project ID for the given access grant.
+func GetPublicID(ctx context.Context, config uplink.Config, access *uplink.Access) (id uuid.UUID, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	info, err := getProjectInfo(ctx, config, access)
+	if err != nil {
+		return id, convertKnownErrors(err, "", "")
+	}
+	copy(id[:], info.ProjectPublicId)
+
+	return id, nil
+}
+
+// GetProjectInfo gets the project info for the given access grant.
+func GetProjectInfo(ctx context.Context, config uplink.Config, access *uplink.Access) (_ Info, err error) {
+	defer mon.Task()(&ctx)(&err)
+
+	info, err := getProjectInfo(ctx, config, access)
+	if err != nil {
+		return Info{}, convertKnownErrors(err, "", "")
+	}
+	var id uuid.UUID
+	copy(id[:], info.ProjectPublicId)
+
+	projectInfo := Info{
+		PublicId: id,
+		Salt:     info.ProjectSalt,
+	}
+
+	if info.EdgeUrlOverrides != nil {
+		projectInfo.EdgeUrlOverrides = &EdgeURLOverrides{
+			AuthService:         string(info.EdgeUrlOverrides.AuthService),
+			PublicLinksharing:   string(info.EdgeUrlOverrides.PublicLinksharing),
+			InternalLinksharing: string(info.EdgeUrlOverrides.PrivateLinksharing),
+		}
+	}
+
+	return projectInfo, nil
+}
+
+//go:linkname convertKnownErrors xlr8d.io/uplink.convertKnownErrors
+func convertKnownErrors(err error, bucket, key string) error
+
+//go:linkname setSatelliteSigner xlr8d.io/uplink.setSatelliteSigner
+func setSatelliteSigner(project *uplink.Project, signer signing.Signer)
+
+// SetSatelliteSigner will enable an uplink Project's configuration to
+// better coordinate with the Satellite by providing the Uplink the Satellite's
+// ability to sign messages.
+func SetSatelliteSigner(project *uplink.Project, signer signing.Signer) {
+	setSatelliteSigner(project, signer)
+}
