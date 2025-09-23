f257a9d87aebd81cdee3efdf26fe912933ebcd09
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2025-09-23 07:49:04 -0400

    Add files via upload

---
+++ upload_test.go
@@ -0,0 +1,170 @@
+// Copyright (C) 2025 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package pieceupload
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/zeebo/errs"
+
+	"xlr8d.io/common/pb"
+	"xlr8d.io/common/oss"
+)
+
+var (
+	fakePrivateKey = mustNewPiecePrivateKey()
+)
+
+func TestUploadOne(t *testing.T) {
+	for _, tc := range []struct {
+		desc           string
+		failPuts       int
+		cancelLongTail bool
+		cancelUpload   bool
+		expectUploaded bool
+		expectNum      int
+		expectErr      string
+		stallManager   *StallManager
+		stallTimeout   time.Duration
+	}{
+		{
+			desc:           "first piece successful",
+			expectUploaded: true,
+			expectNum:      0,
+		},
+		{
+			desc:           "second piece successful",
+			failPuts:       1,
+			expectUploaded: true,
+			expectNum:      1,
+		},
+		{
+			desc:         "upload canceled",
+			cancelUpload: true,
+			expectErr:    "context canceled",
+		},
+		{
+			desc:           "long tail canceled",
+			cancelLongTail: true,
+			expectUploaded: false,
+		},
+		{
+			desc:      "manager fails to return next piece",
+			failPuts:  2,
+			expectErr: "piece limit exchange failed: oh no",
+		},
+		{
+			desc:           "successful upload with stall manager",
+			stallManager:   NewStallManager(),
+			expectUploaded: true,
+			expectNum:      0,
+		},
+		{
+			desc:           "stall timeout triggers context cancellation",
+			stallManager:   NewStallManager(),
+			stallTimeout:   10 * time.Millisecond,
+			expectUploaded: false,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			uploadCtx, uploadCancel := context.WithCancel(context.Background())
+			t.Cleanup(uploadCancel)
+			longTailCtx, longTailCancel := context.WithCancel(context.Background())
+			t.Cleanup(longTailCancel)
+
+			if tc.cancelUpload {
+				uploadCancel()
+			}
+			if tc.cancelLongTail {
+				longTailCancel()
+			}
+
+			manager := newManagerWithExchanger(2, failExchange{})
+
+			var putter PiecePutter
+			if tc.stallTimeout > 0 {
+				// Use slowPutter for stall timeout test
+				putter = &slowPutter{
+					t:     t,
+					delay: 50 * time.Millisecond, // Longer than stall timeout
+				}
+				tc.stallManager.SetMaxDuration(tc.stallTimeout)
+			} else {
+				putter = &fakePutter{t: t, failPuts: tc.failPuts}
+			}
+
+			uploaded, err := UploadOne(longTailCtx, uploadCtx, manager, putter, fakePrivateKey, tc.stallManager)
+			if tc.expectErr != "" {
+				require.EqualError(t, err, tc.expectErr)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tc.expectUploaded, uploaded)
+			if tc.expectUploaded {
+				assertResults(t, manager, revision{0}, makeResult(piecenum{tc.expectNum}, revision{0}))
+			}
+		})
+	}
+}
+
+// slowPutter simulates a slow upload for stall timeout testing
+type slowPutter struct {
+	t     *testing.T
+	delay time.Duration
+}
+
+func (p *slowPutter) PutPiece(longTailCtx, uploadCtx context.Context, limit *pb.AddressedOrderLimit, privateKey oss.PiecePrivateKey, data io.ReadCloser) (*pb.PieceHash, *struct{}, error) {
+	assert.Equal(p.t, fakePrivateKey, privateKey, "private key was not passed correctly")
+
+	// Simulate slow upload
+	time.Sleep(p.delay)
+
+	select {
+	case <-uploadCtx.Done():
+		return nil, nil, uploadCtx.Err()
+	case <-longTailCtx.Done():
+		return nil, nil, longTailCtx.Err()
+	default:
+		num := pieceReaderNum(data)
+		return hash(num), nil, nil
+	}
+}
+
+type fakePutter struct {
+	t        *testing.T
+	failPuts int
+}
+
+func (p *fakePutter) PutPiece(longTailCtx, uploadCtx context.Context, limit *pb.AddressedOrderLimit, privateKey oss.PiecePrivateKey, data io.ReadCloser) (*pb.PieceHash, *struct{}, error) {
+	assert.Equal(p.t, fakePrivateKey, privateKey, "private key was not passed correctly")
+
+	num := pieceReaderNum(data)
+	if p.failPuts > 0 {
+		p.failPuts--
+		return nil, nil, errs.New("put failed for piece: %d", num)
+	}
+
+	select {
+	case <-uploadCtx.Done():
+		return nil, nil, uploadCtx.Err()
+	case <-longTailCtx.Done():
+		return nil, nil, longTailCtx.Err()
+	default:
+		return hash(num), nil, nil
+	}
+}
+
+func mustNewPiecePrivateKey() oss.PiecePrivateKey {
+	pk, err := oss.PiecePrivateKeyFromBytes(bytes.Repeat([]byte{1}, 64))
+	if err != nil {
+		panic(err)
+	}
+	return pk
+}
